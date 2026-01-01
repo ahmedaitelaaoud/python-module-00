7a47b53d80b9d821a6837ee253734d56b9910b53
ahmedaitelaaoud <ahmedaitelaaoud@gmail.com>
2026-01-01 20:06:26 +0100

    Add exercise functions and main testing script

---
+++ ex2/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total():
+    Day_1 = int(input("Day 1 harvest: "))
+    Day_2 = int(input("Day 2 harvest: "))
+    Day_3 = int(input("Day 3 harvest: "))
+    Total_harvest = Day_1 + Day_2 + Day_3
+    print("Total harvest: ", Total_harvest)
