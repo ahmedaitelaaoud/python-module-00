be62e4ff65de1a2313e07d54a7bf726c2c0430b4
ahmedaitelaaoud <ahmedaitelaaoud@gmail.com>
2026-01-08 14:34:25 +0100

    Refactor harvest counting functions for consistency and clarity; update seed inventory function return type

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,5 +1,5 @@
 def ft_count_harvest_iterative():
     N = int(input("Days until harvest: "))
-    for i in range(1,N + 1):
+    for i in range(1, N + 1):
         print("Day ", i)
     print("Harvest time!")
--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,8 +1,12 @@
-def ft_count_harvest_recursive(current_day=1, N=None):
+def rec(current_day=1, N=None):
     if N is None:
         N = int(input("Days until harvest: "))
     if current_day > N:
         print("Harvest time!")
         return
     print("Day", current_day)
-    ft_count_harvest_recursive(current_day + 1, N)
+    rec(current_day + 1, N)
+
+
+def ft_count_harvest_recursive():
+    rec()
